199c8bae54b934988c07a48e2b9b3a631aca6540
djentstorm <52841716+djentstorm@users.noreply.github.com>
2020-06-18 19:38:33 -0400

    Good mod for your game
    I think adding this hentai class to your game could really create a more immersive experience.

---
+++ Hentai.py
@@ -0,0 +1,10 @@
+class Hentai(Good_Porn):
+
+  def __init__(self):
+    self.big_tiddies = True
+    self.tentacles = True
+    
+  def nice(self):
+    while self.big_tiddies and self.tentacles:
+      print("nice")
+      
